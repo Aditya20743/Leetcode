62995fbacc3d48f941ffad735e20298055c15094
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-01-17 22:47:11 +0530

    Time: 22 ms (89.87%), Space: 11.1 MB (52.66%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -9,6 +9,10 @@ public:
                 break;
             }
         }
+        // Don't worry about 0s before 1 as they are at their right place.
+        // FIND index where 1st ONE occurs and store it in j;
+        // keep count of zeros and ones in z and o respectively
+        
         for(int i=j;i<s.size();i++){
             if(s[i]=='0'){
                 z++;
@@ -19,10 +23,11 @@ public:
             if(z>o){
                 z=o;
             }
-            // else if(z<o){
-            //     swap(z,o);
-            // }
         }
         return min(o,z);
+        
+// 00110 ->reduced to 110-> change last 0 to 1 rather than two 1's to 0.
+// 1100011 -> till 4th index, z=3,o=2 now we will swap the values, 
+// to preserve the count of o.
     }
 };
