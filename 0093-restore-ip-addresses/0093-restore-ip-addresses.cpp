de550ebb3dc3b7f77084e0d4b14f67005875a27e
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-01-22 00:04:22 +0530

    Time: 14 ms (7.62%), Space: 11.6 MB (5.01%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<string> res;
+    int n;
+    
+    bool isValid(string temp){
+        if(temp[0]=='0')    return false;
+        int val= stoi(temp);
+        if(val>255) return false;
+        return true;
+    }
+    void solve(int idx, string &s, int parts, string str){
+        
+        if(idx== n && parts==4){
+            str.pop_back();
+            res.push_back(str);
+            return ;
+        }
+        
+        if(idx+1<= n){
+            solve(idx+1,s,parts+1,str +s.substr(idx,1)+".");
+        }
+        if( idx+2 <=n && isValid(s.substr(idx,2)) ){
+            solve(idx+2,s,parts+1,str +s.substr(idx,2)+".");
+        }
+        if( idx+3 <=n && isValid(s.substr(idx,3))){
+            solve(idx+3,s,parts+1,str +s.substr(idx,3)+".");
+        }
+        
+    }
+    vector<string> restoreIpAddresses(string s) {
+        n= s.size();
+        res.clear();
+        string str="";
+        
+        if(n>12) return {};
+        // int idx=0;
+        // int parts=0;
+        
+        solve(0,s,0,str);
+        return res;
+    }
+};
