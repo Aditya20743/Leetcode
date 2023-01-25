90c0f6c19f24266e89cbcf73572545ff1cdb1462
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-01-25 18:58:47 +0530

    Time: 23 ms (86.79%), Space: 13.3 MB (58.46%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,50 +10,30 @@
  */
 class Solution {
 public:
+    // IN min heap we use to write greater while declaring so greater cmp
+    struct cmp{
+        bool operator() (ListNode* a, ListNode* b){
+            return a->val > b->val;
+        }
+    };
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(lists.size()==0) return NULL;
-        else if(lists.size()==1 && lists[0]==NULL)  return NULL; 
+        priority_queue<ListNode* , vector<ListNode*> , cmp > maxheap;
+        ListNode* dummy=new ListNode(-1);
+        ListNode* tail= dummy;
         
-        priority_queue< int> mh;
         for(int i=0;i<lists.size();i++){
-            ListNode* temp= lists[i];
-            while(temp){
-                mh.push(temp->val);
-                temp= temp->next;
-            }
-            // for(int j=0;j<lists[i].size();j++){
-            //     mh.push(list[i][j]->val);
-            // }
+            if(lists[i]!= NULL)
+            maxheap.push(lists[i]);
         }
-        int l= mh.size()-1;
-        vector<int> v(mh.size());
-        while(!mh.empty()){
-            v[l--]= mh.top();
-            mh.pop();
-        }
-        if(v.size()>=2){
-        ListNode* h= new ListNode(v[0]);
-        ListNode* t= new ListNode(v[1]);
-        h->next= t;
-        int i=2;
-        while(i!= v.size()){
-            ListNode* x= new ListNode(v[i]);
-            t->next= x;
-            t=t->next;
-            i++;
-        }
-            
-        t->next= NULL;
-        return h;
-        }
-        else if(v.size()==1){
-            ListNode* h= new ListNode(v[0]);
-            h->next= NULL;
-            return h;
-        }
-        return NULL;
         
-        
+        while(!maxheap.empty()){
+            ListNode* temp= maxheap.top();
+            tail->next= temp;
+            tail= temp;
+            maxheap.pop();
+            if(temp->next)  maxheap.push(temp->next);
+        }
+        return dummy->next;
         
     }
 };
