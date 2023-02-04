a54914590810c0a29c98ddb2762d5dc77521a8ca
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-02-04 11:36:03 +0530

    Time: 496 ms (10.37%), Space: 174.9 MB (5.01%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     bool check(string x,string s){
-        unordered_map<char,int> mp1,mp2;
-        for(auto it: x) mp1[it]++;
-        for(auto it: s) mp2[it]++;
-        return mp1==mp2;
+        //unordered_map<char,int> mp1,mp2;
+        vector<int> b(26),c(26);
+        for(int i=0;i<26;i++){b[i]=0; c[i]=0; }   
+        for(char it: x) b[it-'a']++;
+        for(char it: s) c[it-'a']++;
+        return b==c;
     }
     bool checkInclusion(string s1, string s2) {
         unordered_map<char,int> mp;
+        int a[26];
+        for(int i=0;i<26;i++)   a[i]=0;
+        
         int z= s1.size();
         if(z>s2.size()) return false;
-        for(auto it: s1)    mp[it]++;
+        for(char it: s1)    a[it-'a']++;
         
         for(int i=0;i<s2.size()-z+1;i++){
-            if(mp[s2[i]]>0){
+            if(a[s2[i]-'a']>0){
                 string x= s2.substr(i,z);
                 if(check(x,s1)) return true;
             }
