f1298c134ff716edb6c9c727f504e5aabffd3ee2
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-02-08 16:08:43 +0530

    Time: 135 ms (73.63%), Space: 50.6 MB (40.91%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void dfs(int node,vector<int>&vis, vector<int> adj[]  ){
+        vis[node]=1;
+        for(auto it: adj[node]){
+            if(!vis[it]){
+                dfs(it,vis,adj);
+            }
+        }
+    }
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        int size= connections.size();
+        if(size< n-1)   return -1;
+        
+        vector<int> adj[n];
+        for(int i=0;i<size;i++){
+            adj[connections[i][0]].push_back(connections[i][1]);
+            adj[connections[i][1]].push_back(connections[i][0]);
+        }
+        
+        vector<int> vis(n,0);
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(i,vis,adj);
+                vis[i]=1;
+                count++;
+            }
+        }
+        return count-1;
+    }
+};
