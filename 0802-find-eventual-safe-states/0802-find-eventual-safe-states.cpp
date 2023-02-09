1dfc304ad27a734cdb70d44f375c2e5dca95ab80
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-02-09 13:59:17 +0530

    Time: 155 ms (88.80%), Space: 48.4 MB (60.24%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,40 +1,43 @@
 class Solution {
 public:
+    bool dfs(int node,vector<int>&vis, vector<vector<int>>& graph,
+            vector<int>& path,vector<int>& check){
+        vis[node]=1;
+        path[node]=1;
+        
+        
+        for(auto it: graph[node]){
+            if(!vis[it]){
+                if(dfs(it,vis,graph,path,check)==true)
+                    return true;   //cycle present
+            }
+            else if(path[it]==1){
+                return true;  // cycle present
+            }
+        }
+        path[node]=0;
+        check[node]=1;
+        return false;
+    }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int V= graph.size();
-        vector<int> adj[V];
+        int n= graph.size();
+        vector<int> vis(n,0);
+        vector<int> path(n,0);
+        vector<int> check(n,0);
         
-        for(int i=0;i<V;i++){
-            for(int j=0;j<graph[i].size();j++){
-                adj[graph[i][j]].push_back(i);
+        
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(i,vis,graph,path,check);
+                vis[i]=1;
             }
         }
         
-        int outdeg[V];
-        for(int i=0;i<V;i++)    outdeg[i]=0;
         
-        for(int i=0;i<V;i++){
-            for(auto it: adj[i])
-                outdeg[it]++;
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            if(check[i]==1)   ans.push_back(i);
         }
-        queue<int> q;
-        for(int i=0;i<V;i++){
-            if(outdeg[i]==0)    q.push(i);
-        }
-        vector<int> v;
-        while(!q.empty()){
-            int n=q.front();
-            q.pop();
-            v.push_back(n);
-            for(auto it: adj[n]){
-                outdeg[it]--;
-                if(outdeg[it]==0){
-                    q.push(it);
-                }
-            }
-        }
-        sort(v.begin(),v.end());
-        return v;
-        
+        return ans;
     }
 };
