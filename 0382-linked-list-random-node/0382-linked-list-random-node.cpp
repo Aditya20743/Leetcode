fad5f54ad7dc2c6be212c9670cf03f71e5d6a0ae
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-03-10 22:35:47 +0530

    Time: 23 ms (68.11%), Space: 16.6 MB (85.75%) - LeetHub

--- 0382-linked-list-random-node/0382-linked-list-random-node.cpp
+++ 0382-linked-list-random-node/0382-linked-list-random-node.cpp
@@ -8,23 +8,46 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
-    vector<int> store;
+    //Note : head is guaranteed to be not null, so it contains at least one node.
+    ListNode* HeadNode;
     Solution(ListNode* head) {
-        ListNode* temp=head;
-        while(temp){
-            store.push_back(temp->val);
-            temp=temp->next;
-        }
+       HeadNode = head;
     }
-    
+    // Reservoir sampling
+    //returns value of a random node
     int getRandom() {
-        long long lb=0, ub= store.size()-1;
-        long long x=(rand()%(ub-lb+1)+lb);
-        return store[x];
+        int res, len = 1;
+        ListNode* x = HeadNode;
+        while(x){
+            if(rand() % len == 0){
+                res = x->val;
+            }
+            len++;
+            x = x->next;
+        }
+        return res;
     }
 };
+// class Solution {
+// public:
+//     vector<int> store;
+//     Solution(ListNode* head) {
+//         ListNode* temp=head;
+//         while(temp){
+//             store.push_back(temp->val);
+//             temp=temp->next;
+//         }
+//     }
+    
+//     int getRandom() {
+//         long long lb=0, ub= store.size()-1;
+//         long long x=(rand()%(ub-lb+1)+lb);
+//         return store[x];
+//     }
+// };
 
 /**
  * Your Solution object will be instantiated and called as such:
