b53082ce581d7aad675086677374920ad1881030
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-03-11 14:56:10 +0530

    Time: 3 ms (83.82%), Space: 6.7 MB (33.60%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    bool isSafe(int row,int col,vector<string>&board,int n){
+        int dr= row, dc= col;
+        // left
+        while(dc>=0){
+            if(board[dr][dc]=='Q'){
+                return false;
+            }
+            dc--;
+        }
+        dr= row, dc=col;
+        //upper diagonal
+        while(dc>=0 && dr>=0){
+            if(board[dr][dc]=='Q'){
+                return false;
+            }
+            dc--;  dr--;
+        }
+        dr= row, dc=col;
+        //lower diagonal
+        while(dc>=0 && dr<n){
+            if(board[dr][dc]=='Q'){
+                return false;
+            }
+            dc--;  dr++;
+        }
+        return true;
+        
+    }
+    
+    void solve(vector<string>&board,vector<vector<string>>&ans, int n, int col ){
+        if(col==n){
+            ans.push_back(board);
+            return ;
+        }
+        for(int row=0;row<n;row++){
+            if(isSafe(row,col,board,n)){
+                board[row][col]='Q';
+                solve(board,ans,n,col+1);
+                board[row][col]='.';
+            }
+        }
+    }
+    int totalNQueens(int n) {
+        vector<vector<string>> ans;
+        string s(n,'.');
+        vector<string> board;
+        for(int i=0;i<n;i++)    board.push_back(s);
+        
+        solve(board,ans,n,0);
+        return ans.size();
+    }
+//     int totalNQueens(int n) {
+        
+//     }
+};
