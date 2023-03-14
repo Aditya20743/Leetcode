779d672080ee03db62620c86afa305efaaa8816c
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-03-14 19:33:47 +0530

    Time: 11 ms (38.13%), Space: 15.2 MB (24.92%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -13,7 +13,19 @@ public:
         }
     }
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>> dp(prices.size()+1, vector<int>(2,-1));
-        return helper(0,1,prices,dp);
+        vector<vector<int>> dp(prices.size()+1, vector<int>(2,0));
+        dp[prices.size()][0]=0;
+        dp[prices.size()][1]=0;
+        dp[0][0]=0;
+        int n= prices.size();
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<2;j++){
+                if(j)
+                    dp[i][j]= max(dp[i+1][0]-prices[i], dp[i+1][1]);
+                else 
+                    dp[i][j]= max(dp[i+1][1]+prices[i], dp[i+1][0]);
+            }
+        }
+        return dp[0][1];
     }
 };
