71e655651a65788947f49ac9066903de68cb82d0
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-03-16 11:30:24 +0530

    Time: 27 ms (35.17%), Space: 11.7 MB (18.33%) - LeetHub

--- 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
@@ -20,34 +20,19 @@ public:
 
 class Solution {
 public:
-//     void helper(Node* root, vector<int>&ans){
-//         if(!root)   return ;
+    void helper(vector<int>&v ,Node* root){
+        if(!root)    return;
+        v.push_back(root->val);
         
-//     }
-    
-    vector<int> preorder(Node* root) {
-        vector<int> ans;
-        // helper(root,ans);
-        // return ans;
-        if(!root)   return ans;
-        stack<Node*> st;
-        st.push(root);
-        
-        while(!st.empty()){
-            Node* t= st.top();
-            ans.push_back(t->val);
-            st.pop();
-            vector<Node*>  c= t->children;
-            int n=c.size();
-            for(int i=n-1;i>=0;i--){
-                
-                if(c[i]){
-                    st.push(c[i]);
-                }
-            }
-            
+        vector<Node*> c= root->children;
+        for(Node* it: c){
+            helper(v, it);
         }
-        return ans;
         
+    }
+    vector<int> preorder(Node* root) {
+        vector<int> v;
+        helper(v, root);
+        return v;
     }
 };
