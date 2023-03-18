20598020fdb0cb87a0ecb87049ca041a160a50a3
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-03-19 00:11:29 +0530

    Time: 367 ms (80.00%), Space: 103.4 MB (40.00%) - LeetHub

---
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    static bool comp(pair<long long,long long>&a,pair<long long,long long>&b){
+        if(a.first==b.first)return a.second<b.second;
+        return a.first<b.first;
+    }
+    #define ll long long
+    long long findScore(vector<int>& nums) {
+        vector<pair<ll,ll>> v(nums.size());
+        for(int i=0;i<nums.size();i++){
+            v[i]= {nums[i],i};
+        }
+        
+        vector<ll> unm(nums.size(),0);
+        
+        sort(v.begin(),v.end(),comp);
+        
+        ll idx=0,c=0,ans=0;
+        while( idx< nums.size()){
+            ll ele= v[idx].first;
+            ll index= v[idx].second;
+            
+            if(unm[index]==0){
+            ans+= ele;
+            unm[index]=1;
+            if(index-1 != -1)
+                unm[index-1]=1;
+                
+            if(index+1 != nums.size())
+                unm[index+1]=1;
+             }
+            idx++;
+        }
+        return ans;
+    }
+};
