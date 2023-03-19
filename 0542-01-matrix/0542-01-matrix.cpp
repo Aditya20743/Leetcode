216040346b56600d5297c29e99d0f36cb1f39689
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-03-19 16:10:09 +0530

    Time: 89 ms (63.41%), Space: 31.8 MB (53.60%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,46 +1,44 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n=mat.size();
-        int m=mat[0].size();
+        int m= mat.size();
+        int n= mat[0].size();
         
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        vector<vector<int>> ans(n,vector<int>(m,0));
+        vector<vector<int>> dis(m,vector<int>(n,INT_MAX));
+        
         
         queue<pair<pair<int,int>, int>> q;
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(mat[i][j]==0){
+                    dis[i][j]=0;
                     q.push({{i,j},0});
-                    ans[i][j]=0;
-                    vis[i][j]=1;
                 }
             }
         }
-        
-        int dir[5]={-1,0,1,0,-1};
         
         while(!q.empty()){
             int r= q.front().first.first;
             int c= q.front().first.second;
-            int dis=q.front().second;
+            int d= q.front().second;
+            
             q.pop();
-            // vis[i][j]=1;
+            
+            int rd[4]= {-1,0,1,0};
+            int cd[4]= {0,1,0,-1};
             
             for(int i=0;i<4;i++){
-                int nr= r + dir[i];
-                int nc= c + dir[i+1];
-                    
-                if(nr>=0 && nc>=0 && nr< n && nc< m){
-                    if( vis[nr][nc]==0 && mat[nr][nc]==1){
-                        q.push({{nr,nc}, dis+1});
-                        vis[nr][nc]=1;
-                        ans[nr][nc]=dis+1;
-                    }
+                int nr= r+ rd[i];
+                int nc= c+ cd[i];
+                
+                if(nr>=0 && nr<m && nc>=0 && nc<n){
+                    if(mat[nr][nc]==1 && dis[nr][nc]> d+1 ){
+                        dis[nr][nc]= d+1;
+                        q.push({{nr,nc},d+1});
+                    } 
                 }
-            }
+            } 
         }
-        return ans;
+        return dis;
     }
 };
