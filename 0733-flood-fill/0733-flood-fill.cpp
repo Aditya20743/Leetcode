f9a0459dd62512657a7276a60180d17d4746e6f9
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-03-19 15:40:24 +0530

    Time: 14 ms (30.10%), Space: 14.3 MB (28.63%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,34 +1,43 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+    int rd[4]={-1,0,1,0};
+    int cd[4]={0,1,0,-1};
+    
+    void dfs(int r, int c, int col, vector<vector<int>>& image, vector<vector<int>> &vis,int original_col){
         int m= image.size();
-        int n=image[0].size();
-        vector<vector<int>> visited(m,vector<int>(n,0));
+        int n= image[0].size();
         
-        queue<pair<int,int>> q;
-        q.push({sr,sc});
-        int old= image[sr][sc];
-        int dir[5]={-1,0,1,0,-1};
+        vis[r][c]=1;
+        image[r][c]=col;
         
-        if(old == color)    return image;
-        
-        while(!q.empty()){
-            int r= q.front().first;
-            int c= q.front().second;
-            q.pop();
-            visited[r][c]=1;
-            image[r][c]=color;
+        for(int i=0;i<4;i++){
+            int nr= r+ rd[i];
+            int nc= c+ cd[i];
             
-            for(int i=0;i<4;i++){
-                int nr= r+dir[i];
-                int nc= c+dir[i+1];
-                
-                if(nr>=0 && nc>=0 && nr<m && nc<n && visited[nr][nc]==0 
-                   && image[nr][nc]== old){
-                    q.push({nr,nc});
+            if(nr>=0 && nr<m && nc>=0 && nc<n ){
+                if(vis[nr][nc]==0 && image[nr][nc]==original_col){
+                    //image[nr][nc]=col;
+                    //vis[nr][nc]=1;
+                    
+                    dfs(nr,nc,col,image,vis, original_col);
                 }
             }
+            
         }
+        
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int m= image.size();
+        int n= image[0].size();
+        
+        //int original= 
+        
+        if(color== image[sr][sc]){
+            return image;
+        }
+        vector<vector<int>> vis(m,vector<int>(n,0));
+        
+        dfs(sr,sc, color, image, vis, image[sr][sc] );
         return image;
     }
 };
