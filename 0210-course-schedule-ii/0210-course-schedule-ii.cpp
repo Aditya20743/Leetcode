fffa15afa03d3e5927d9cdcc6fa1e4865773a540
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-03-20 16:32:30 +0530

    Time: 25 ms (52.79%), Space: 13.2 MB (93.04%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites){ 
-        vector<int> adj[numCourses];
+    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
+        vector<int> adj[n];
         for(int i=0;i<prerequisites.size();i++){
             adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
         }
+        vector<int> indeg(n,0);
         
-        queue<int> q;
-        int indeg[numCourses];
-        for(int i=0;i<numCourses;i++)   indeg[i]=0;
-        
-        for(int i=0;i<numCourses;i++){
-            for(auto it: adj[i])
+        for(int i=0;i<n;i++){
+            for(auto it: adj[i]){
                 indeg[it]++;
-        }
-        
-        for(int i=0;i<numCourses;i++){
-            if(indeg[i]==0) q.push(i);
-        }
-        
-        vector<int> ans;
-        while(!q.empty()){
-            int n= q.front();
-            q.pop();
-            ans.push_back(n);
-            
-            for(auto it: adj[n]){
-                indeg[it]--;
-                if(indeg[it]==0)    q.push(it);
             }
         }
-        if(ans.size() ==numCourses)  return ans; 
-            return {};
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(indeg[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int> ans;
+        while(!q.empty()){
+            int node= q.front();
+            q.pop();
+            ans.push_back(node);
+            
+            for(auto it: adj[node]){
+                indeg[it]--;
+                if(indeg[it]==0){
+                    q.push(it);
+                }
+            }
+            
+        }
+        if(ans.size()!= n)  return {};
+        return ans;
     }
 };
