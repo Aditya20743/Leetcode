cda7fe0fc077c391695ffb61e1e3cc85071abcb1
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-03-20 16:58:49 +0530

    Time: 231 ms (21.70%), Space: 58.9 MB (33.78%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,43 +1,41 @@
 class Solution {
 public:
-    bool dfs(int node,vector<int>&vis, vector<vector<int>>& graph,
-            vector<int>& path,vector<int>& check){
-        vis[node]=1;
-        path[node]=1;
-        
-        
-        for(auto it: graph[node]){
-            if(!vis[it]){
-                if(dfs(it,vis,graph,path,check)==true)
-                    return true;   //cycle present
-            }
-            else if(path[it]==1){
-                return true;  // cycle present
-            }
-        }
-        path[node]=0;
-        check[node]=1;
-        return false;
-    }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n= graph.size();
-        vector<int> vis(n,0);
-        vector<int> path(n,0);
-        vector<int> check(n,0);
+        int n =graph.size();
+        vector<int> revg[n], indeg(n,0);
         
         
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(i,vis,graph,path,check);
-                vis[i]=1;
+            for( auto it:graph[i]){
+                revg[it].push_back(i);
             }
         }
         
-        
-        vector<int> ans;
         for(int i=0;i<n;i++){
-            if(check[i]==1)   ans.push_back(i);
+            for(auto it: revg[i]){
+                indeg[it]++;
+            }
         }
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(indeg[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int> ans;
+        while(!q.empty()){
+            int node= q.front();
+            ans.push_back(node);
+            q.pop();
+            
+            for(auto it: revg[node]){
+                indeg[it]--;
+                if(indeg[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        sort(ans.begin(),ans.end());
         return ans;
     }
 };
