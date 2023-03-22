cc504e0fe488215cf3f93eca28fec7b366a0fc88
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-03-22 10:38:40 +0530

    Time: 111 ms (32.00%), Space: 19.3 MB (91.58%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,41 +1,35 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
+        if(grid[0][0]!= 0)  return -1;
+        int n= grid.size();
+        int m= grid[0].size();
         
-        if(grid[0][0]==1 || grid[n-1][m-1]==1)   return -1;
-        if(grid.size()==1)  return 1;   
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        
-        queue<pair<int, pair<int,int> > > q;
-        q.push({0,{0,0}});
-        
-        vis[0][0]=1;
-        while(!q.empty()){       
-            int steps= q.front().first;
-            int r= q.front().second.first;
-            int c= q.front().second.second;
+        vector<vector<int>> dis(n,vector<int>(m,INT_MAX));
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>> > pq;
+        pq.push({1,{0,0}});
+        dis[0][0]=1;
+        while(!pq.empty()){
+            int dist= pq.top().first;
+            int row= pq.top().second.first;
+            int col= pq.top().second.second;
+            pq.pop();
             
-            q.pop();
-            vis[r][c]=1;
-        
             for(int i=-1;i<=1;i++){
                 for(int j=-1;j<=1;j++){
-                    int nr= r+ i;
-                    int nc= c+ j;
-                    if(nr>=0 && nc>=0 && nr<n && nc<m){
-                        if(nr== n-1 && nc== m-1 ){
-                            return steps+1+1;
-                        }
-                        if(grid[nr][nc]==0 && vis[nr][nc]==0){
-                            q.push({steps+1,{nr,nc}});
-                            vis[nr][nc]=1;
+                    int nr= row+ i;
+                    int nc= col+ j;
+                    if( nr>=0 && nc>=0 && nr<n && nc<m){
+                        if(grid[nr][nc]==0 && dis[nr][nc]> dist+1){
+                            dis[nr][nc]=dist+1;
+                            pq.push({dist+1,{nr,nc}});
                         }
                     }
                 }
             }
         }
-        return -1;
+        if(dis[n-1][m-1]==INT_MAX)  return -1;
+        return dis[n-1][m-1];
+        
     }
 };
