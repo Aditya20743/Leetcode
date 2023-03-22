72eb63fc39ae696684e28ea2f88c5b2186ee2324
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-03-22 23:07:32 +0530

    Time: 33 ms (54.92%), Space: 16.4 MB (15.16%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -5,28 +5,29 @@ public:
         for(int i=0;i<flights.size();i++){
             adj[flights[i][0]].push_back({flights[i][1],flights[i][2]});
         }
-        vector<vector<int>> dis(n,vector<int>(k+2,INT_MAX));
-        dis[src][0]= 0;
+        vector<int> dis(n,INT_MAX);
+        dis[src]= 0;
         
-        queue<pair<int,int>> q;
-        q.push({src,0});
+        queue<pair<int,pair<int,int>>> q;
+        q.push({0,{src,0}});
         
         while(!q.empty()){
-            int node= q.front().first;
-            int steps= q.front().second;
+            int node= q.front().second.first;
+            int steps= q.front().first;
+            int distance= q.front().second.second;
             q.pop();
             
             for(auto it: adj[node]){
                 int adjn= it[0];
                 int adjW= it[1];
                 
-                if(steps<k+1 &&  dis[adjn][steps+1] > dis[node][steps]+ adjW){
-                    dis[adjn][steps+1]= dis[node][steps]+ adjW;
-                    q.push({adjn,steps+1});
+                if(steps<k+1 &&  dis[adjn] > distance+ adjW){
+                    dis[adjn]= distance + adjW;
+                    q.push({steps+1,{adjn,dis[adjn]}});
                 }
             }
         }
-        int ans= INT_MAX;
+        //int ans= INT_MAX;
         
         // for(int i=0;i<n;i++){
         //     for(int j=0;j<k+2;j++){
@@ -34,11 +35,11 @@ public:
         //     }
         //     cout<<endl;
         // }
-        for(int i=0;i<=k+1;i++){
-            if(dis[dst][i] != INT_MAX){
-                ans= min(ans,dis[dst][i]);
-            }
-        }
-        return ans== INT_MAX ? -1 : ans; 
+        // for(int i=0;i<=k+1;i++){
+        //     if(dis[dst][i] != INT_MAX){
+        //         ans= min(ans,dis[dst][i]);
+        //     }
+        // }
+        return dis[dst] == INT_MAX ? -1 : dis[dst]; 
     }
 };
