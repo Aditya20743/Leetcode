26da376f939e0097c64e990aaade82a38f72f1c7
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-03-25 14:16:59 +0530

    Time: 47 ms (62.99%), Space: 19 MB (11.03%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
+    bool check(int mid, vector<int>&piles, int h){
+        long long t=0;
+        for(int i=0;i<piles.size();i++){
+            double it= piles[i];
+            t+= ceil(it/mid);
+        }
+        if(t<= h)   return true;
+        return false;
+    }
     int minEatingSpeed(vector<int>& piles, int h) {
-        // long long sum=0;
-        // for(auto it: piles){
-        //     sum+= it;
-        // }
-        long long l=1,r=*max_element(piles.begin(),piles.end());
-        long long m=0;
-        while(l<=r){
-            m= l+ (r-l)/2;
-            long long nh=0;
-            for(auto it: piles){
-                double x = it;
-                nh+= ceil(x/m);
-            }
-            if(nh>h){
-                l=m+1;
+        long long ul= *max_element(piles.begin(),piles.end()), ll= 1;
+        long long mid=0;
+        while(ul>= ll){
+            mid= (ul+ll)/2;
+            
+            if(check(mid,piles,h)){
+                ul = mid-1;
             }
             else{
-                r=m-1;
+                ll= mid+1;
             }
         }
-        //ut<<m<<" ";
-        // int mh=0;
-        // for(int i=m-1;i>=1;i--){
-        //     for(auto it: piles){
-        //         long double x= it;
-        //         mh+= ceil(x/i);
-        //     }
-        //     if(mh> h)   return i+1;  
-        //     mh=0;
-        // }
-        return l;
+        return ul+1;
     }
 };
