e1101d3a1d5a67beaa5bc3247867f4886e62d842
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-03-25 13:02:27 +0530

    Time: 418 ms (34.91%), Space: 122.2 MB (40.82%) - LeetHub

--- 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.cpp
+++ 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
     int findSmallestInteger(vector<int>& nums, int value) {
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            // while(nums[i]<0){
-            //     nums[i]+= value;
-            // }
-            if(nums[i]%value<0){
-                nums[i]= nums[i]%value+ value;
-            }
-            else nums[i]= nums[i]%value;
-        }
-        for(auto it: nums)  cout<<it<<" ";
-        sort(nums.begin(),nums.end());
-        map<int,int> mp;
-        for(auto it: nums){
-            mp[it]++;
-        }
         
-        int c=0,i=0;
-        while(c<nums.size()){
-            if(mp[c%value]>0){
-                mp[c%value]--;
-                if(mp[c%value]==0)    mp.erase(c%value);
-                c++;
+        map<int,int> mp;
+        if(value==1) return nums.size();
+        for(int i=0;i<nums.size();i++){
+            while(nums[i]<0){
+                nums[i]= nums[i]%value + value;
+            }
+            nums[i]= nums[i]%value;
+            mp[nums[i]]++;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(mp[i%value]==0){
+                return i;
             }
             else{
-                break;
+                mp[i%value]--;
             }
         }
-        return c;
+        return nums.size();
     }
 };
