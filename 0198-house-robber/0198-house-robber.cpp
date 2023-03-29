8c038526cca28c2683088677c77936fcb8f5cf2b
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-03-29 12:53:57 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (62.49%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,28 +2,16 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n= nums.size();
-        vector<int> dp(n);
-        
-        // dp[i] will store max money robbed till index i
-        
-        // if only one house => we will rob it. 
-        dp[0]= nums[0]; 
-    
-        // start from index 1 till last house index
-        // each house can be robbed or skipped
-        
-        for(int i=1;i<n;i++){
-            // if house is robbed, then money of that house (nums[i]) will be added to amount robbed till now that is (dp[i-2]) because we can rob house consecutively  
-            
+        vector<int>dp(n+2,0);
+        for(int i=n-1;i>=0;i--){
             int pick= nums[i];
-            // if house is not robbed/picked, dp[i] = dp[i-1]
-            int not_pick= dp[i-1];
-            if(i>=2)    
-            // if i<2 then there will be not money robbed so we will add dp[i-2] only whe i>=2
-                pick+= dp[i-2];
-            dp[i]= max(pick , not_pick);
+            //if(i<n-1)
+                pick+= dp[i+2];
+            
+            int not_pick= 0;
+            not_pick+= dp[i+1];
+            dp[i]= max(pick,not_pick);
         }
-        // finally we will return max money robbed till dp[n-1]
-        return dp[n-1];
+        return dp[0];
     }
 };
