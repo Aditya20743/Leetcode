a14a85d0820e2c6536bc22af8355b8aeffb5f661
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-05-15 15:56:33 +0530

    Time: 16 ms (11.79%), Space: 10.2 MB (5.57%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        int m=grid.size();
+        int m= grid.size();
         int n=grid[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,0));
         
-        dp[0][0]=grid[0][0];
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        dp[0][0]= grid[0][0];
         
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                int left=INT_MAX,up=INT_MAX;
-                if(i-1>=0){
-                    left= dp[i-1][j];
+                
+                if(i==0 && j!=0){
+                    dp[0][j]=  grid[0][j]+(dp[0][j-1]);
                 }
-                if(j-1>=0){
-                    up= dp[i][j-1];
+                else if(j==0 && i!=0){
+                    dp[i][0]= grid[i][0]+ dp[i-1][0]; 
                 }
-                if(!(i==0 && j==0))
-                dp[i][j]= min(left, up)+ grid[i][j];
+                
+                else if(i>0 && j>0)
+                    dp[i][j]= grid[i][j] +min(dp[i-1][j], dp[i][j-1]);
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                cout<<dp[i][j]<<" ";
-            }
-            cout<<endl;
-        }
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
         return dp[m-1][n-1];
     }
 };
