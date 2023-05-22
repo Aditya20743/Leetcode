55cfed8e52fdaa61aeefde2b072714627a384173
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-05-23 00:08:34 +0530

    Time: 3 ms (54.50%), Space: 5.9 MB (93.53%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     int reverse(int x) {
-        long long y= abs(x);
-        long long ans=0;
+        int y= abs(x);
+        int ans=0;
         while(y>0){
+            if(ans>INT_MAX/10 || ans<INT_MIN/10)  return 0;
             int r=y%10;
             y= y/10;
             ans= ans*10+ r;
+            
         }
         if(x<0){
             ans= -ans;
         }
         
-        long long upper= pow(2,31)-1;
-        long long lower= - pow(2,31);
-        if(ans> upper){
-            ans=0;
-        }
-        else if(ans< lower){
-            ans=0;
-        }
         // cout<<ans<<endl;
-        return (int)ans;
+        return ans;
     }
 };
