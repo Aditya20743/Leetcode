ad914183c3a9cd38b6d31f75797f84856dc7ded7
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-05-23 23:49:02 +0530

    Time: 8 ms (55.56%), Space: 13.4 MB (28.21%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -4,51 +4,49 @@ public:
         int m= grid.size();
         int n= grid[0].size();
         
-        queue<pair <pair<int,int>, int> > q;
-        
         vector<vector<int>> vis(m,vector<int>(n,0));
+        
+        queue< pair<pair<int,int>, int> > q;
         
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==2){
-                    q.push({{i,j},0});
+                if(!vis[i][j] && grid[i][j]==2){
                     vis[i][j]=1;
+                    q.push({{i,j},0});
                 }
             }
         }
+        int tmax= 0;
         int dir[5]={-1,0,1,0,-1};
         
-        int max_t=0;
-        
         while(!q.empty()){
+            
             int r= q.front().first.first;
             int c= q.front().first.second;
             int t= q.front().second;
-            
-            max_t= max(t,max_t);
-            
             q.pop();
             
+            tmax = max(tmax,t);
             for(int i=0;i<4;i++){
                 int nr= r+ dir[i];
                 int nc= c+ dir[i+1];
-                if(nr>=0 && nc>=0 && nr<m && nc<n){
-                    if(grid[nr][nc]==1 && vis[nr][nc]==0){
+                
+                if(nr>=0 && nc>=0 && nr< m && nc< n){
+                    if(!vis[nr][nc] && grid[nr][nc]==1){
                         vis[nr][nc]=1;
-                        grid[nr][nc]=2;
                         q.push({{nr,nc},t+1});
                     }
                 }
             }
+            
         }
-        
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(vis[i][j]==0 && grid[i][j]==1){
-                    return -1;
+                if(!vis[i][j] && grid[i][j]==1){
+                return -1;
                 }
             }
         }
-        return max_t;
+        return tmax;
     }
 };
