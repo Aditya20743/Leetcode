f0890584bf391e6cc1374c0e430e322614f78bb3
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-05-27 10:49:46 +0530

    Time: 6 ms (75.16%), Space: 13.4 MB (27.99%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,52 +1,49 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int m= grid.size();
+        int m=grid.size();
         int n= grid[0].size();
-        
         vector<vector<int>> vis(m,vector<int>(n,0));
         
-        queue< pair<pair<int,int>, int> > q;
-        
+        queue<pair<int,pair<int,int>>> q;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(!vis[i][j] && grid[i][j]==2){
+                if(grid[i][j] == 2){
+                    q.push({0,{i,j}});
                     vis[i][j]=1;
-                    q.push({{i,j},0});
                 }
             }
         }
-        int tmax= 0;
-        int dir[5]={-1,0,1,0,-1};
         
+        vector<int> dir= {-1,0,1,0,-1};
+        int t_max=0;
+                           
         while(!q.empty()){
-            
-            int r= q.front().first.first;
-            int c= q.front().first.second;
-            int t= q.front().second;
+            int r= q.front().second.first;
+            int c= q.front().second.second;
+            int t= q.front().first;
+            vis[r][c]=1;
             q.pop();
             
-            tmax = max(tmax,t);
+            t_max=max(t_max, t);
+            
             for(int i=0;i<4;i++){
                 int nr= r+ dir[i];
                 int nc= c+ dir[i+1];
                 
-                if(nr>=0 && nc>=0 && nr< m && nc< n){
-                    if(!vis[nr][nc] && grid[nr][nc]==1){
-                        vis[nr][nc]=1;
-                        q.push({{nr,nc},t+1});
-                    }
+                if(nr>=0 && nc>=0 && nr<m && nc<n && !vis[nr][nc] && grid[nr][nc]==1){
+                    q.push({t+1,{nr,nc}});
+                    vis[nr][nc]=1;
                 }
             }
-            
         }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(!vis[i][j] && grid[i][j]==1){
-                return -1;
+                    return -1;
                 }
             }
         }
-        return tmax;
+        return t_max;
     }
 };
