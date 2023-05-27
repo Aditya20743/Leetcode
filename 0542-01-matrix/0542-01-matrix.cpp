9ba94788ca28eef18fce19eda55d75960e6f735f
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-05-27 11:40:01 +0530

    Time: 93 ms (57.73%), Space: 31.5 MB (60.25%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -4,41 +4,45 @@ public:
         int m= mat.size();
         int n= mat[0].size();
         
-        vector<vector<int>> dis(m,vector<int>(n,INT_MAX));
+        vector<vector<int>> ans(m,vector<int>(n,1e6));
         
+        // vector<vector<int>> vis(m,vector<int>(n,-1));
         
-        queue<pair<pair<int,int>, int>> q;
+        queue<pair<int,pair<int,int>>> q;
+        
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(mat[i][j]==0){
-                    dis[i][j]=0;
-                    q.push({{i,j},0});
+                if(mat[i][j]== 0){
+                    ans[i][j]=0;
+                    q.push({0,{i,j}});
+                    // vis[i][j] = 1;
                 }
             }
         }
+        vector<int> dir={-1,0,1,0,-1};
         
         while(!q.empty()){
-            int r= q.front().first.first;
-            int c= q.front().first.second;
-            int d= q.front().second;
+            int d= q.front().first;
+            int r= q.front().second.first;
+            int c= q.front().second.second;
             
             q.pop();
             
-            int rd[4]= {-1,0,1,0};
-            int cd[4]= {0,1,0,-1};
-            
             for(int i=0;i<4;i++){
-                int nr= r+ rd[i];
-                int nc= c+ cd[i];
+                int nr= r+ dir[i];
+                int nc= c+ dir[i+1];
                 
-                if(nr>=0 && nr<m && nc>=0 && nc<n){
-                    if(mat[nr][nc]==1 && dis[nr][nc]> d+1 ){
-                        dis[nr][nc]= d+1;
-                        q.push({{nr,nc},d+1});
-                    } 
+                if(nr>=0 && nc>=0 && nr< m && nc< n){
+                    // cout<<mat[nr][nc]<<endl;
+                    if(ans[nr][nc]> d+1){
+                        ans[nr][nc]= d+1;
+                        q.push({d+1,{nr,nc}});
+                    }
                 }
-            } 
+                
+                // if(!vis[nr][nc])
+            }
         }
-        return dis;
+        return ans;
     }
 };
