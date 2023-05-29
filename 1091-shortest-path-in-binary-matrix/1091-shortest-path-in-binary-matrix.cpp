9e3112d7a43248a2c84802ff82a8729a634554f1
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-05-29 15:07:46 +0530

    Time: 130 ms (24.84%), Space: 19.2 MB (93.54%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        if(grid[0][0]!= 0)  return -1;
+        priority_queue< pair<int,pair<int,int>> ,vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>> > pq;
         int n= grid.size();
-        int m= grid[0].size();
         
-        vector<vector<int>> dis(n,vector<int>(m,INT_MAX));
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>> > pq;
-        pq.push({1,{0,0}});
-        dis[0][0]=1;
+        if(grid[0][0]== 1)  return -1;
+        
+        pq.push({0,{0,0}});
+        
+        vector<vector<int>> dis(n,vector<int>(n,INT_MAX));
+        dis[0][0]=0;
+        
         while(!pq.empty()){
-            int dist= pq.top().first;
-            int row= pq.top().second.first;
-            int col= pq.top().second.second;
+            int r= pq.top().second.first;
+            int c= pq.top().second.second;
+            int d= pq.top().first;
             pq.pop();
+            
+            if( r== n-1 && c==n-1 ){
+                return d+1 ;
+            }
             
             for(int i=-1;i<=1;i++){
                 for(int j=-1;j<=1;j++){
-                    int nr= row+ i;
-                    int nc= col+ j;
-                    if( nr>=0 && nc>=0 && nr<n && nc<m){
-                        if(grid[nr][nc]==0 && dis[nr][nc]> dist+1){
-                            dis[nr][nc]=dist+1;
-                            pq.push({dist+1,{nr,nc}});
+                    int nr= r+i;
+                    int nc= c+j;
+                    if(!(i== 0 && j==0) && nr>=0 && nc>=0 && nr<n && nc<n ){
+                        if(grid[nr][nc]==0 && dis[nr][nc]> dis[r][c] + 1){
+                            dis[nr][nc]= dis[r][c] + 1;
+                            pq.push({dis[nr][nc],{nr,nc}});
                         }
                     }
                 }
             }
         }
-        if(dis[n-1][m-1]==INT_MAX)  return -1;
-        return dis[n-1][m-1];
-        
+        return dis[n-1][n-1] == INT_MAX ? -1 : dis[n-1][n-1];
     }
 };
