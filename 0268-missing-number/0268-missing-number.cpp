5dc6e393b99d880ba7530cc256652d5250a9b3dc
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-05-31 15:40:28 +0530

    Time: 15 ms (92.22%), Space: 17.8 MB (92.07%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,9 +3,16 @@ public:
     int missingNumber(vector<int>& nums) {
         int sum=0;
         int n= nums.size();
-        for(auto it: nums){
-            sum+= it;
+        // for(auto it: nums){
+        //     sum+= it;
+        // }
+        // return n*(n+1)/2 - sum;
+        
+        int xorr=n;
+        for(int i=0;i<n;i++){
+            xorr= xorr^nums[i];
+            xorr^= i;
         }
-        return n*(n+1)/2 - sum;
+        return xorr;
     }
 };
