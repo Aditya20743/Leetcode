c486358a141051e37207db6e15cc2a23e3012a30
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-05-31 17:13:33 +0530

    Time: 3 ms (85.24%), Space: 9.4 MB (95.11%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m=matrix.size();
-        int n=matrix[0].size();
+        int m= matrix.size();
+        int n= matrix[0].size();
         
-        int rl=0, rr=n-1;
-        int cu=0, cd=n-1;
+        int row= m-1;
+        int col= 0;
         
-        while( cu<= m-1  && rr> -1){
-            if(matrix[cu][rr]==target)  return true;
-            else if(matrix[cu][rr]> target) rr--;
-            else
-                cu++;
+        while(row>=0 && col< n){
+            
+            while(row>=0 && col< n && matrix[row][col] > target){
+                row--;
+            }
+            while(row>=0 && col< n && matrix[row][col]< target){
+                col++;
+            }
+            if(row>=0 && col< n && matrix[row][col]==target)    return true;
         }
-        return 
-            false;
-        
-//         while(cu<cd){
-//             int cm= cu+ (cd-cu)/2;
-//             if(matrix[cm][rl]<target)   cu= cm;
-//             else if(matrix[cm][rl]> target) cd= cm-1;
-//             else 
-//                 return true;
-//         }
-        
-//         while(rl<=rr){
-//             int rm= rl+ (rr-rl)/2;
-//             if(matrix[cu][rm]>target)   rr= rm-1;
-//             else if(matrix[cu][rm]< target) rl= rm+1;
-//             else 
-//                 return true;
-//         }
-//         return false;
+        return false;
     }
 };
