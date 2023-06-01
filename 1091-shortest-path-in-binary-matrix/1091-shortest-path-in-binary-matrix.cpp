5d35d4acebcd4c11286ff81304ad446d957e82aa
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-06-01 09:42:14 +0530

    Time: 72 ms (71.64%), Space: 21.6 MB (53.45%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        priority_queue< pair<int,pair<int,int>> ,vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>> > pq;
         int n= grid.size();
         
-        if(grid[0][0]== 1)  return -1;
+        if(grid[0][0]==1)   return -1;
         
-        pq.push({0,{0,0}});
+        vector<vector<int>> vis(n,vector<int>(n,0));
         
-        vector<vector<int>> dis(n,vector<int>(n,INT_MAX));
-        dis[0][0]=0;
+        queue<pair<pair<int,int>,int>> q;
+        q.push({{0,0},0});
+        vis[0][0]=1;
         
-        while(!pq.empty()){
-            int r= pq.top().second.first;
-            int c= pq.top().second.second;
-            int d= pq.top().first;
-            pq.pop();
+        while(!q.empty()){
+            int r= q.front().first.first;
+            int c= q.front().first.second;
+            int d= q.front().second;
             
-            if( r== n-1 && c==n-1 ){
-                return d+1 ;
-            }
+            if(r==n-1 && c== n-1)   return d+1;
+            q.pop();
             
             for(int i=-1;i<=1;i++){
                 for(int j=-1;j<=1;j++){
                     int nr= r+i;
                     int nc= c+j;
-                    if(!(i== 0 && j==0) && nr>=0 && nc>=0 && nr<n && nc<n ){
-                        if(grid[nr][nc]==0 && dis[nr][nc]> dis[r][c] + 1){
-                            dis[nr][nc]= dis[r][c] + 1;
-                            pq.push({dis[nr][nc],{nr,nc}});
+                    if(nr>=0 &&nc>=0 && nr<n &&nc<n && !(i==0 && j==0)){
+                        if(grid[nr][nc]==0 && !vis[nr][nc]){
+                            q.push({{nr,nc},d+1});
+                            vis[nr][nc]=1;
                         }
                     }
                 }
             }
         }
-        return dis[n-1][n-1] == INT_MAX ? -1 : dis[n-1][n-1];
+        return -1;
     }
 };
