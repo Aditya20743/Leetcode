dd8f4bc43a56c973e7fd452ae98c9832b0b920e7
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-06-06 14:29:29 +0530

    Time: 20 ms (41.40%), Space: 19.8 MB (76.78%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,34 +1,58 @@
 class Solution {
 public:
+    
     int trap(vector<int>& height) {
-        // stack<int> st;
+        
+        //optimised
+        // two pointer approach
         int n= height.size();
-        vector<int> pre,suf;
-        int maxi=0;
-        for(int i=0;i<n;i++){
-            maxi=max(maxi,height[i]);
-            pre.push_back(maxi);
-        }
-        maxi=0;
-        for(int i=n-1;i>=0;i--){
-            maxi=max(maxi,height[i]);
-            suf.push_back(maxi);
-        }
-        reverse(suf.begin(),suf.end());
-        int ans=0;
-
-        // for(auto it: pre){
-        //     cout<<it<<" ";
-        // }
-        // cout<<endl;
-        // for(auto it: suf){
-        //     cout<<it<<" ";
-        // }
-        for(int i=0;i<n;i++){
-            
-            ans+= min(suf[i],pre[i])-height[i];
+        int left=0,right=n-1, ans=0;
+        int lmax=0,rmax=0;
+        
+        while(left<= right){
+            if(height[left]< height[right]){
+                if(height[left]>lmax){
+                    lmax= height[left];
+                }
+                else{
+                    ans+= lmax- height[left];
+                }
+                left++;
+            }
+            else{
+                if(height[right]>rmax){
+                    rmax= height[right];
+                }
+                else{
+                    ans+= rmax- height[right];
+                }
+                right--;
+            }
         }
         return ans;
+        
+        
+        // brute/ naive approach
+//         int n= height.size();
+//         vector<int> pre,suf;
+//         int maxi=0;
+//         for(int i=0;i<n;i++){
+//             maxi=max(maxi,height[i]);
+//             pre.push_back(maxi);
+//         }
+//         maxi=0;
+//         for(int i=n-1;i>=0;i--){
+//             maxi=max(maxi,height[i]);
+//             suf.push_back(maxi);
+//         }
+//         reverse(suf.begin(),suf.end());
+//         int ans=0;
+
+//         for(int i=0;i<n;i++){
+            
+//             ans+= min(suf[i],pre[i])-height[i];
+//         }
+//         return ans;
         
     }
 };
