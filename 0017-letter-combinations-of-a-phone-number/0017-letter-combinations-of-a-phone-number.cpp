54ad1fb83dc425dd2af82331b83e58e8b581073c
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-06-13 23:39:48 +0530

    Time: 4 ms (36.33%), Space: 6.8 MB (13.99%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    void helper(int i, int j, string digits, vector<string>&ans, vector<string>&chars,
+               string &temp){
+        
+        if(i== digits.size()){
+            // if(temp.size()!=0)
+            ans.push_back(temp);
+            return;
+        }
+        if(j== chars[digits[i]-'0'].size()-1){
+            temp+= chars[digits[i]-'0'][j];
+            helper( i+1 ,0 , digits, ans, chars, temp);
+            temp.erase(temp.begin()+temp.size()-1);
+        }
+        else{
+            temp+= chars[digits[i]-'0'][j];
+            helper(i+1, 0, digits, ans, chars, temp); //take
+            temp.erase(temp.begin()+temp.size()-1);
+
+            helper(i, j+1, digits, ans, chars, temp ); // not_take
+  
+        }
+        
+    }
+    
+    vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+        if(digits.size()==0)
+            return ans;
+        string temp="";
+        vector<string> chars(10);
+
+        chars[2]="abc";
+        chars[3]="def";
+        chars[4]="ghi";
+        chars[5]="jkl";
+        chars[6]="mno";
+        chars[7]="pqrs";
+        chars[8]="tuv";
+        chars[9]="wxyz";
+        
+        helper(0, 0, digits, ans, chars, temp);
+        
+        return ans;
+    }
+};
