09b9ac2b444e53e7b8acddae71fd1494a00a7ddb
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-06-14 11:43:15 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (15.30%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
-    int dp[46];
     int climbStairs(int n) {
-        dp[1]= 1;
-        dp[2]= 2;
+        if(n<=2)    return n;
+        vector<int> dp(n+1);
+        
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        
         for(int i=3;i<=n;i++){
-            dp[i]= dp[i-1]+ dp[i-2];
+            dp[i]= dp[i-1] + dp[i-2];
         }
+        
         return dp[n];
     }
 };
