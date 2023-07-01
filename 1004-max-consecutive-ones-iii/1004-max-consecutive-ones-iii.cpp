977f588fb53d2055e6db8d7ab14781d2cdca1e6f
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-07-01 17:32:04 +0530

    Time: 49 ms (99.50%), Space: 55.2 MB (99.97%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,34 +1,71 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        int i=0,j=0,c=0,ans=0,z=0;
-        while(j<nums.size() && i<=j){
-            if(nums[j]){
-                j++;
-                c++;
-                ans=max(ans,c+z);
-            }
-            else{
-                z++;
-                j++;
-                if(z<=k)
-                ans=max(ans,c+z);
-                else{
-                    while(z>k){
-                        if(nums[i]==1){
-                            c--;
-                        }
-                        else{
-                            z--;
-                        }
-                        i++;
-                    }
-                    ans=max(ans,c+z);
+        int i=0,j=0, ans=0;
+        int c=0,n=nums.size();
+        
+        while(j<n){
+            while(j<n && c<=k){
+                if(j<n &&nums[j]==0){
+                    c++;
                 }
                 
+                if(c>k) break;
+                j++;
+                ans=max(ans,j-i);
+                
             }
+            ans=max(ans,j-i);
             
+            while(i<=j && c>k){
+                if(nums[i]==0){
+                    c--;
+                }
+                i++;
+            }
+            ans=max(ans,j-i);
+            j++;
         }
+        
         return ans;
+//       [1,1,1,0,0,0,1,0,1,0,0,1,1,1,1,0,1,0,1,1,1,0,1,1,0,1,0,1,1,0,1,1,0,1,1,1,0,0,0,1,0,0,0,1,1,1,1,0,0,1]
+// 10  
+        
+        
+//         while(j<n && c<k){
+//             ans= max(ans,j);
+//             if(nums[j]==0){
+//                 c++;
+//             }
+//             j++;
+//         }
+        
+        
+//         while(j<n){
+//             if(c<=k){
+//                 if(nums[j]==0){
+//                     c++;
+//                     ans= max(ans,j-i);
+//                 }
+//                 j++;
+//             }
+//             else{
+//                 while(i<j && c>k){
+//                     if(nums[i]==0){
+//                         c--;
+//                     }
+//                     i++;
+//                 }
+//                 if(i==j){
+//                     j++;
+//                     i++;
+//                 }
+//                 // ans= max(ans,j-i+1);
+//             }
+//             j++;
+//         }
+//         // cout<<endl;
+//         return ans;
+        
     }
 };
