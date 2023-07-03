f15ac572e7501fa1df002600ef63e64dd0d051a6
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-07-03 10:53:49 +0530

    Time: 26 ms (16.71%), Space: 14.3 MB (47.25%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -28,21 +28,43 @@ public:
     }
     
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(p==q)    return p;
-        
-        vector<TreeNode*> path1;
-        path_finder(root, p, path1);
-        
-        vector<TreeNode*> path2;
-        path_finder(root, q, path2);
-        
-        cout<<path1.size()<<" "<<path2.size()<<endl;
-        
-        for(int i=0;i<min(path1.size(),path2.size());i++){
-            if(path1[i]!= path2[i]){
-                return path1[i-1];
-            }
+        // TC O(N)  SC O(H) auxilary stack space SC O(N)->worst case in skew trees
+        if(root==NULL || root==p || root==q){
+            return root;
         }
-        return path1[min(path1.size(), path2.size())-1];
+        // if we get p or q we return it;
+        
+        TreeNode* left= lowestCommonAncestor(root->left, p, q);
+        TreeNode* right= lowestCommonAncestor(root->right, p, q);
+        
+        if(left== NULL && right){
+            return right;
+        }
+        else if(!right && left){
+            return left;
+        }
+        else if(!right && !left){
+            return NULL;
+        }
+        else{
+            return root;
+        }
+//         TC O(N)   SC O(2H) -> store path 
+//         if(p==q)    return p;
+        
+//         vector<TreeNode*> path1;
+//         path_finder(root, p, path1);
+        
+//         vector<TreeNode*> path2;
+//         path_finder(root, q, path2);
+        
+//         cout<<path1.size()<<" "<<path2.size()<<endl;
+        
+//         for(int i=0;i<min(path1.size(),path2.size());i++){
+//             if(path1[i]!= path2[i]){
+//                 return path1[i-1];
+//             }
+//         }
+//         return path1[min(path1.size(), path2.size())-1];
     }
 };
