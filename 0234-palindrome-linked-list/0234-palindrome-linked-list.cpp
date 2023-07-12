3b89eb7922e1086497dad86fc1248feae59c439e
Aditya20743 <79308024+Aditya20743@users.noreply.github.com>
2023-07-12 15:51:07 +0530

    Time: 250 ms (69.78%), Space: 116.5 MB (66.37%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,31 +10,64 @@
  */
 class Solution {
 public:
+    // 1 stack store first half then traverse and check with stack.top
+    // 2 reverse linkedlist from mid then check from head and mid
+    
+    ListNode* reverse(ListNode* head, ListNode* prev){
+        
+        if(head){
+            ListNode* temp= reverse(head->next, head);
+            head->next= prev;
+            
+            return temp;
+        }
+        return prev;
+    }
+    
     bool isPalindrome(ListNode* head) {
         
         if(!head || !head->next)   return true;
         
+        
+        
         ListNode* slow= head, *fast= head;
-        int n=0;
-        stack<ListNode*> st;
         
         while(fast && fast->next){
-            st.push(slow);
             slow= slow->next;
             fast= fast->next->next;
-            
         }
-        // cout<<st.size()<<endl;
-    
-        while(slow){
-            // cout<<st.top()->val<<" "<<slow->val<<endl;
-            if(st.top()->val== slow->val){
-                st.pop();
-            }
-            slow= slow->next;
-        }
-        // cout<<st.size()<<endl;
-        return st.empty();
         
+        ListNode* mid= reverse(slow,NULL);
+        
+        while(mid ){
+            if(mid->val != head->val){
+                return false;
+            }
+            mid=mid->next;
+            head= head->next;
+        }
+        return true;
     }
+//         int n=0;
+//         stack<ListNode*> st;
+        
+//         while(fast && fast->next){
+//             st.push(slow);
+//             slow= slow->next;
+//             fast= fast->next->next;
+            
+//         }
+//         // cout<<st.size()<<endl;
+    
+//         while(slow){
+//             // cout<<st.top()->val<<" "<<slow->val<<endl;
+//             if(st.top()->val== slow->val){
+//                 st.pop();
+//             }
+//             slow= slow->next;
+//         }
+//         // cout<<st.size()<<endl;
+//         return st.empty();
+        
+//     }
 };
