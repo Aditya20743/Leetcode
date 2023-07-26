175b8ede8963ce93cbfac95cd0c452676067bfb2
Aditya Khadekar <79308024+Aditya20743@users.noreply.github.com>
2023-07-26 17:15:45 +0530

    Time: 109 ms (82.76%), Space: 43.4 MB (98.98%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
+    // Brute force
     int longestConsecutive(vector<int>& nums) {
-        if(nums.size()==0)  return 0;
-        
-        unordered_set<int> s;
-        for(auto num: nums){
-            s.insert(num);
-        }
-        
-        int maxi=1,streak=1;
-        for(auto num: nums){
-            if(!s.count(num-1)){
-                int cnum= num+1;
-                streak=1;
-                while(s.count(cnum)){
-                    streak++;
-                    cnum++;
-                }
-                maxi=max(maxi, streak);
-                streak=0;
+        sort(nums.begin(),nums.end());
+        int n= nums.size();
+        if(n==0)    return 0;
+        int c=1;
+        int maxi=1;
+        for(int i=1;i<n;i++){
+            if(nums[i]== nums[i-1]+1){
+                c++;
             }
+            else if(nums[i]==nums[i-1]){
+                continue;
+            }
+            else{
+                maxi= max(maxi , c);
+                c=1;
+            }
+        }
+        if(c!= 1){
+            maxi= max(maxi,c);
         }
         return maxi;
     }
