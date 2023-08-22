4bff1518d2ce775f1671568c22bd04ef0ae257a6
Aditya Khadekar <79308024+Aditya20743@users.noreply.github.com>
2023-08-22 15:55:40 +0530

    Time: 297 ms (71.43%), Space: 116.8 MB (97.50%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,37 +1,39 @@
 class MedianFinder {
 public:
-    priority_queue<int, vector<int>, greater<int> > minHeap;
-	priority_queue<int> maxHeap;
-    MedianFinder(){}
-
+    priority_queue<int> max_heap;
+    priority_queue<int , vector<int> , greater<int> > min_heap;
+    
+    MedianFinder() {
+        
+    }
+    
     void addNum(int num) {
-        if (maxHeap.empty() or maxHeap.top() > num) {
-			maxHeap.push(num);
-		} else {
-			minHeap.push(num);
-		}
-
-		if (maxHeap.size() > minHeap.size() + 1) {
-			minHeap.push(maxHeap.top());
-			maxHeap.pop();
-		} else if (minHeap.size() > maxHeap.size() + 1) {
-			maxHeap.push(minHeap.top());
-			minHeap.pop();
-		}
+        
+        if(max_heap.empty() || max_heap.top() > num){
+            max_heap.push(num);
+        }
+        else if(max_heap.top()<= num){
+            min_heap.push(num);
+        }
+        
+        if(max_heap.size() > min_heap.size() + 1 ){
+            min_heap.push(max_heap.top());
+            max_heap.pop();
+        }
+        else if(min_heap.size() > max_heap.size()){
+            max_heap.push(min_heap.top());
+            min_heap.pop();
+        }
     }
     
     double findMedian() {
-        if (maxHeap.size() == minHeap.size()) {
-			if (maxHeap.empty()) {
-				return 0;
-			} else {
-				double avg = (maxHeap.top() + minHeap.top()) / 2.0;
-				return avg;
-			}
-
-		} else {
-			return maxHeap.size() > minHeap.size() ? maxHeap.top() : minHeap.top();
-		}
+        if(max_heap.size()== min_heap.size()){
+            return (1.0*max_heap.top() + 1.0*min_heap.top())/2;
+        }
+        // else if(max_heap.size()> min_heap.size()){
+        //     return 1.0*max_heap.top();
+        // }
+        return max_heap.top();
     }
 };
 
