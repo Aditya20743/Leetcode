31816db41d64b88cdea5c848533f133136a45fcb
Aditya Khadekar <79308024+Aditya20743@users.noreply.github.com>
2023-08-28 10:40:18 +0530

    Time: 3 ms (36.88%), Space: 6.8 MB (25.78%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,35 +1,57 @@
 class MyStack {
 public:
-    queue<int> q1;
     MyStack() {
         
     }
+    queue<int> q1, q2;
     
     void push(int x) {
-        
         q1.push(x);
-        for(int i=0;i<q1.size()-1;i++){
-            q1.push(q1.front());
-            q1.pop();
-        }
     }
     
     int pop() {
         
-        int x=-1;
-        if(!q1.empty()){
-            x= q1.front();
+        while(q1.size() > 1){
+            int element= q1.front();
             q1.pop();
+            
+            q2.push(element);
         }
-        return x;
+        
+        int top= q1.front();
+
+        q1.pop();
+        
+        
+        while(!q2.empty()){
+            int element= q2.front();
+            q2.pop();
+            
+            q1.push(element);
+        }
+        
+        return top;
     }
     
     int top() {
-       int x=-1;
-        if(!q1.empty()){
-            x= q1.front();
+        while(q1.size() > 1){
+            int element= q1.front();
+            q1.pop();
+            
+            q2.push(element);
         }
-        return x;
+        int top= q1.front();
+        q1.pop();
+        q2.push(top);
+        
+        while(!q2.empty()){
+            int element= q2.front();
+            q2.pop();
+            
+            q1.push(element);
+        }
+        
+        return top;
     }
     
     bool empty() {
