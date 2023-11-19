3ba010467775cb64e5c0e601ac16a10d12d8983d
Aditya Khadekar <79308024+Aditya20743@users.noreply.github.com>
2023-11-19 12:10:31 +0530

    Time: 4 ms (50.00%), Space: 6.5 MB (16.67%) - LeetHub

---
+++ 2939-maximum-xor-product/2939-maximum-xor-product.cpp
@@ -0,0 +1,22 @@
+typedef long long ll;
+class Solution {
+public:
+    int maximumXorProduct(long long a, long long b, int n) {
+        ll mod= 1e9+7;
+        for(int i=n-1;i>=0;i--){
+            ll bit= 1LL* 1<<i;
+            if((bit & a) == (bit & b)){
+                a = a|bit;
+                b = b|bit;
+            }
+            else{
+                if(a>b) swap(a,b);
+                
+                a = a|bit;
+                b = b & (~bit);
+                
+            }
+        }
+        return ((a%mod)*(b%mod))%mod;
+    }
+};
