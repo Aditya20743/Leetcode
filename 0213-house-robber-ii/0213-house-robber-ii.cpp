6fdfb10a5fa6382d210d33917e70ff8440e71520
Aditya Khadekar <79308024+Aditya20743@users.noreply.github.com>
2023-11-23 14:08:19 +0530

    Time: 4 ms (29.01%), Space: 8.3 MB (47.80%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
-    int helper(vector<int>& nums, int start, int end){
-        vector<int> dp(nums.size());
-        dp[start]= nums[start];
-        for(int i=start+1;i<=end;i++){
-            int pick= nums[i];
-            int not_pick= dp[i-1];
-            if(i>=start+2){
-                pick+= dp[i-2];
-            }
-            //cout<< pick<<" "<<not_pick<<endl;
-            dp[i]= max(pick,not_pick);
-        }
-        return dp[end];
+    int helper(int i,int n,vector<int>&nums,vector<int>&dp){
+        if(i>n) return 0;
+        
+        if(dp[i]!= -1)  return dp[i];
+        
+        int take= helper(i+2,n,nums,dp)+nums[i];
+        int not_take= helper(i+1,n,nums,dp);
+        
+        return dp[i]= max(take,not_take);
+    }
+    int helper2(int i,int n,vector<int>&nums,vector<int>&dp2){
+        if(i>n) return 0;
+        
+        if(dp2[i]!= -1)  return dp2[i];
+        
+        int take= helper(i+2,n,nums,dp2)+nums[i];
+        int not_take= helper(i+1,n,nums,dp2);
+        
+        return dp2[i]= max(take,not_take);
     }
     int rob(vector<int>& nums) {
-        // 0 - n-2
-        // 1 - n-1
         int n= nums.size();
-        if(n==1) return nums[0];
-        return max(helper(nums,0,n-2), helper(nums,1,n-1));
+        
+        if(n==1)    return nums[0];
+        
+        vector<int> dp(n,-1);
+        
+        int first= helper(0,n-2,nums,dp);
+        
+        vector<int> dp2(n,-1);
+        int second= helper2(1,n-1,nums,dp2);
+        
+        return max(first, second);
     }
 };
