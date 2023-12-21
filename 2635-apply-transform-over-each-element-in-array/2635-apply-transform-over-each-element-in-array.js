2332ec0189ac25519118711d59134bc1440b4507
Aditya Khadekar <79308024+Aditya20743@users.noreply.github.com>
2023-12-21 15:07:17 +0530

    Time: 60 ms (8.08%), Space: 41.9 MB (53.31%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -5,11 +5,15 @@
  */
 var map = function(arr, fn) {
     const returnedArray =[];
-    for(let i=0;i<arr.length;i++){
-        returnedArray[i] = fn(arr[i],i);
-    }
     
+    arr.forEach((ele, index)=>{
+        returnedArray[index] = fn(arr[index], index);
+    })
     
+    // for(let i=0;i<arr.length;i++){
+    //     returnedArray[i] = fn(arr[i],i);
+    // }
+
     
     // does not work -> const returndArray = arr.forEach(fn);
     // const returnedArray = arr.map(fn);
