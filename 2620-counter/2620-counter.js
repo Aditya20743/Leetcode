032bdc5aa9482d9490949dd2f708c31f6f3344ad
Aditya Khadekar <79308024+Aditya20743@users.noreply.github.com>
2023-12-21 11:47:52 +0530

    Time: 52 ms (43.73%), Space: 42.1 MB (29.91%) - LeetHub

---
+++ 2620-counter/2620-counter.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    let t=0;
+    return ()=> n+(t++);
+    // {    
+    // return n+(t++);
+    // };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
