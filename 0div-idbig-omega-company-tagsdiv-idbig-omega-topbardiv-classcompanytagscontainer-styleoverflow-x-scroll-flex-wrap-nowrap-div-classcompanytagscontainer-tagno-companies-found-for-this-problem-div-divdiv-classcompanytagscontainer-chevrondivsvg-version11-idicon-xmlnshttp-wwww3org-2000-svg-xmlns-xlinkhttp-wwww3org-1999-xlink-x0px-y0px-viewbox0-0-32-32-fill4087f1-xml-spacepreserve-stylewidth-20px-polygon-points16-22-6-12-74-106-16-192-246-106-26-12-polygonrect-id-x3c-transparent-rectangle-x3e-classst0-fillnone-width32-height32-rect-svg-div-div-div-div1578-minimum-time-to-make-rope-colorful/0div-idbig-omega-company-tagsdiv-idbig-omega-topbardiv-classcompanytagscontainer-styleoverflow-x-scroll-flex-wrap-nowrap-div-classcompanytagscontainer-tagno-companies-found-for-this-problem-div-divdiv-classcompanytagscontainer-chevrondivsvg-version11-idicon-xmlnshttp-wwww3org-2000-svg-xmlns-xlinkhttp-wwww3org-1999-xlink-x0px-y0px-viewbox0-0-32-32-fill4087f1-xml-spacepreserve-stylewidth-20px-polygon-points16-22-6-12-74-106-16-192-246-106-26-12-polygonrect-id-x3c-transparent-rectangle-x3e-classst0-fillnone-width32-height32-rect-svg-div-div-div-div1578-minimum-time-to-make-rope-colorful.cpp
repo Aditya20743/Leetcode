7893397ce24e0147a1b4d9f0c8d608145844baf6
Aditya Khadekar <79308024+Aditya20743@users.noreply.github.com>
2023-12-27 11:28:20 +0530

    Time: 120 ms (55.29%), Space: 95.7 MB (49.48%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1578-minimum-time-to-make-rope-colorful/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1578-minimum-time-to-make-rope-colorful.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1578-minimum-time-to-make-rope-colorful/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,60 +1,16 @@
 class Solution {
 public:
-//     int helper(int i, string colors, vector<int>&neededTime, vector<int>&dp){
-//         if(i==colors.size()){
-//             return 0;
-//         }
-        
-//         if(dp[i]!= -1)  return dp[i];
-        
-//         if(i!=0 && colors[i]== colors[i-1]){
-//             int f= helper(i+2, colors, neededTime,dp)+ neededTime[i-1];
-//             int s= helper(i+2, colors, neededTime,dp)+ neededTime[i];
-            
-//             return dp[i]=min(f,s);
-//         }
-        
-//         return dp[i]=helper(i+1, colors, neededTime,dp);
-//     }
-    
     int minCost(string colors, vector<int>& neededTime) {
-        // vector<int> dp(colors.size()+1,-1);
+        int mini=0;
         
-        // helper(0,colors, neededTime, dp);
-        
-        // for(auto it: dp){
-        //     cout<<it<<" ";
-        // }
-        // cout<<endl;
-        
-        //aaabbbabbbb
-        
-        priority_queue<int, vector<int> , greater<int> > pq;
-        int c=0, ans=0;
         for(int i=1;i<colors.size();i++){
-            if(colors[i]== colors[i-1] ){
-                if(c==0 )
-                pq.push(neededTime[i-1]);
-                pq.push(neededTime[i]);
-                c++;
+            if(colors[i]==colors[i-1]){
+                mini += min(neededTime[i-1], neededTime[i]);
+                neededTime[i] = max(neededTime[i-1], neededTime[i]);
             }
-            else {
-                c=0;
-                while(pq.size()>1){
-                    ans+= pq.top();
-                    pq.pop();
-                }
-                if(pq.size())   pq.pop();   
-            }
-            // cout<<i<<endl;
+    
         }
         
-        while(pq.size()>1){
-            ans+= pq.top();
-            pq.pop();
-        }
-        // pq.pop();   
-        
-        return ans;
+        return mini;
     }
 };
