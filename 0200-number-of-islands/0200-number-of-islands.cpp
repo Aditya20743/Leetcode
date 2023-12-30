226f1afe1fc8b97f4ff8c394db90b0455a5f9d3a
Aditya Khadekar <79308024+Aditya20743@users.noreply.github.com>
2023-12-30 11:47:41 +0530

    Time: 22 ms (96.33%), Space: 13.8 MB (40.77%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,36 +1,44 @@
 class Solution {
 public:
-    void dfs(int r,int c,vector<vector<int>>& vis, vector<vector<char>>&grid){
-        int m= grid.size();
-        int n=grid[0].size();
-        vis[r][c]=1;
+    int dir[5] = {-1,0,1,0,-1};
+    
+    void dfs(int i,int j,vector<vector<char>>& grid, vector<vector<int>>&vis ){
+        vis[i][j]=1;
         
-        int dir[5]={-1,0,1,0,-1};
-        for(int i=0;i<4;i++){
-            int nr= dir[i]+ r;
-            int nc= dir[i+1] + c;
-            if(nr>=0 && nc>=0 && nr< m && nc<n){
-                if(!vis[nr][nc] && grid[nr][nc]=='1'){
-                    dfs(nr,nc,vis,grid);
+        int n= grid.size();
+        int m= grid[0].size();
+        
+        for(int x=0;x<4;x++){
+            int ni= dir[x]+ i;
+            int nj= dir[x+1]+ j;
+            
+            if(ni>=0 && nj>=0 && ni<n && nj<m ){
+                if(vis[ni][nj]==-1 && grid[ni][nj]=='1'){
+                    vis[ni][nj]=1;
+                    dfs(ni,nj,grid, vis);
                 }
             }
         }
+        
     }
+    
     int numIslands(vector<vector<char>>& grid) {
-        int m= grid.size();
-        int n=grid[0].size();
+        int n= grid.size();
+        int m= grid[0].size();
         
-        vector<vector<int>> vis(m,vector<int>(n,0));
-        int count=0;
+        vector<vector<int>> vis(n, vector<int>(m,-1));
         
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(!vis[i][j] && grid[i][j]=='1'){
-                    dfs(i,j,vis,grid);
-                    count++;
+        int islands=0;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(vis[i][j]== -1 && grid[i][j]=='1'){
+                    dfs(i,j,grid,vis);
+                    islands++;
                 }
             }
         }
-        return count;
+        
+        return islands;
     }
 };
