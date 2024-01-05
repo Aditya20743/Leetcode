b6e80e9fc9e8f9bd0388ff1633b927cfdf83b15c
Aditya Khadekar <79308024+Aditya20743@users.noreply.github.com>
2024-01-05 19:37:27 +0530

    Time: 5 ms (78.84%), Space: 7.7 MB (44.24%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,18 +2,23 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         vector<int>freq (26,0);
+        
+        if(s.size()!= t.size())     return false;
+        
+        // rat , cat r=1, a=1, t=1,  =>  c==0 => false;
+        
         for(auto it: s){
             freq[it-'a']++;
         }
         for(auto it: t){
-            freq[it-'a']--;
-        }
-        
-        for(int i=0;i<26;i++){
-            if(freq[i]){
+            
+            if(freq[it-'a']==0){
                 return false;
             }
+            freq[it-'a']--;
+        
         }
+        
         return true;
     }
 };
