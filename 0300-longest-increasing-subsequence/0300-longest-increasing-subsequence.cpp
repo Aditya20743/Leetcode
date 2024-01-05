56983800ca1043c7197999aa602c15b651159e59
Aditya Khadekar <79308024+Aditya20743@users.noreply.github.com>
2024-01-05 13:59:43 +0530

    Time: 5 ms (92.10%), Space: 10.7 MB (92.65%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
-    int helper(int i, int prev, vector<int>&nums, vector<vector<int>>&dp){
-        if(i>= nums.size()){
-            return 0;
-        }
-        if( dp[i][prev+1]!= -1)    return dp[i][prev+1];
-        
-        int take =0, not_take=0;
-        
-        if( prev==-1 || nums[prev]< nums[i]){
-            take= helper(i+1, i, nums,dp)+1;
-        }
-        not_take = helper(i+1, prev, nums,dp);
-        
-        return dp[i][prev+1]= max(take, not_take);
-    }
     int lengthOfLIS(vector<int>& nums) {
-        
-        int n= nums.size();
-        vector<vector<int>> dp(n, vector<int>(n+1,-1));
-        return helper(0,-1,nums,dp );
+        vector<int> store;
+        for(auto it: nums){
+            if(store.empty() || store.back()<it){
+                store.push_back(it);
+            }
+            else{
+                auto t= lower_bound(store.begin(), store.end(), it);
+                *t= it;
+            }
+        }
+        return store.size();
     }
 };
