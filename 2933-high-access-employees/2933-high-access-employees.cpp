d7f01d4096f4e43bb98d427a1fba52273c8901cc
Aditya Khadekar <79308024+Aditya20743@users.noreply.github.com>
2024-01-13 12:15:36 +0530

    Time: 76 ms (43.31%), Space: 53.6 MB (7.44%) - LeetHub

--- 2933-high-access-employees/2933-high-access-employees.cpp
+++ 2933-high-access-employees/2933-high-access-employees.cpp
@@ -1,44 +1,26 @@
 class Solution {
 public:
-    static bool cmp(vector<string> a, vector<string> b){
-        if(a[0]>b[0]){
-            return false;
-        }
-        else if(a[0]<b[0])  return true;
-        
-        else {
-            return a[1]<b[1];
-        }
-    }
-    int time(string s){
-        int res=0,hr=0;
-        
-        hr+= (s[0]-'0')*10;
-        hr+= (s[1]-'0');
-        
-        res+= (s[2]-'0')*10 + (s[3]-'0') + hr*60;
-    
-        return  res;
-    }
-    
-    
     vector<string> findHighAccessEmployees(vector<vector<string>>& a) {
         vector<string> ans;
         
-        sort(a.begin(), a.end(), (cmp));
-        
-        unordered_map<string, int> mp;
+        map<string,vector<string>> mp;
         
         if(a.size()<3)  return ans;
         
-        for(int i=0;i<a.size()-2;i++){
-            if(mp.find(a[i][0])== mp.end() && a[i][0] == a[i+2][0]){
-                int t2= time(a[i+2][1]);
-                int t1= time(a[i][1]);
-                
-                if(t2-t1 <60){
-                    mp[a[i][0]]++;
-                    ans.push_back(a[i][0]);
+        for(auto it: a){
+            mp[it[0]].push_back(it[1]);
+        }
+        
+        for(auto it: mp){
+            vector<string> t= it.second;
+            sort(t.begin(),t.end());
+            // cout<<t.size()<<" ";
+            if(t.size()<3)  continue;
+            for(int i=0;i<t.size()-2;i++){
+                // cout<<stoi(t[i+2])<<" ";
+                if(stoi(t[i+2])- stoi(t[i]) <100){
+                    ans.push_back(it.first);
+                    break;
                 }
             }
         }
