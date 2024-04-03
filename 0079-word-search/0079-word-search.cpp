d23c2f699e659b9c7083a5490c1296fd99bb267b
Aditya Khadekar <79308024+Aditya20743@users.noreply.github.com>
2024-04-03 10:08:02 +0530

    Time: 258 ms (73.75%), Space: 9.7 MB (78.94%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,52 +1,51 @@
 class Solution {
 public:
-    vector<int> xdir={-1,0,1,0};
-    vector<int> ydir={0,-1,0,1};
+    vector<int> dir = {-1,0,1,0,-1};
     
-    bool solve(int r,int c,vector<vector<char>>& board, string word, int idx, vector<vector<int>>& vis){
-        int m= board.size();
-        int n= board[0].size();
-        
-        vis[r][c]=1;
+    bool functionToFindWord(int i,int j, string&word, vector<vector<char>>&board, int idx, vector<vector<int>>&vis){
+        int m = board.size();
+        int n =board[0].size();
         
         
-        if(idx== word.size()){
+        if(idx == word.size()){
             return true;
         }
         
-        for(int i=0;i<4;i++){
-            int nr= r+ xdir[i];
-            int nc= c+ ydir[i];
+        for(int k=0;k<4;k++){
+            int ni = i+ dir[k];
+            int nj = j+ dir[k+1];
             
-            if(nr>=0 && nc>=0 && nr<m && nc<n ){
-                if(word[idx]== board[nr][nc] && vis[nr][nc]==0){
-                    if(solve(nr,nc,board, word,idx+1,vis)){
+            if(ni>=0 && ni<m && nj>=0 && nj<n && vis[ni][nj]== -1){
+                if(board[ni][nj]== word[idx]){
+                    vis[ni][nj] = 1;
+                    if(functionToFindWord(ni,nj,word,board,idx+1,vis) == true){
                         return true;
                     }
+                    vis[ni][nj] = -1;
                 }
             }
         }
-        vis[r][c]=0;
-        
         return false;
     }
     
+    
     bool exist(vector<vector<char>>& board, string word) {
-        int m= board.size();
-        int n= board[0].size();
-        vector<vector<int>> vis(m,vector<int>(n,0));
+        int m = board.size();
+        int n =board[0].size();
         
+        vector<vector<int>> vis(m,vector<int>(n, -1));
         
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(board[i][j]==word[0]){
-                    if(solve(i,j,board, word, 1,vis)){
+                if(board[i][j] == word[0]){
+                    vis[i][j] = 1;
+                    if(functionToFindWord(i,j,word,board,1, vis) == true){
                         return true;
                     }
+                    vis[i][j] = -1;
                 }
             }
         }
-        
         return false;
     }
 };
