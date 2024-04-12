9dac6270a8ab7606b1fdc11448b1cc763edf2497
Aditya Khadekar <79308024+Aditya20743@users.noreply.github.com>
2024-04-12 10:31:40 +0530

    Time: 10 ms (59.69%), Space: 22.6 MB (41.11%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        int n=height.size();
-        
-        vector<int> greatest_to_left(n);
-        vector<int> greatest_to_right(n);
-        
-        
-        int ans=0;
-        greatest_to_left[0]= height[0];
-        greatest_to_right[n-1]= height[n-1];
-        
-        for(int i=1;i<n;i++){
-            greatest_to_left[i]= max(greatest_to_left[i-1], height[i]);
-            
-            greatest_to_right[n-i-1]=max(greatest_to_right[n-i],height[n-i-1]);
+    int trap(vector<int> A) {
+        int n= A.size();
+        int left=0; int right=n-1;
+        int res=0;
+        int maxleft=0, maxright=0;
+        while(left<=right){
+            if(A[left]<=A[right]){
+                if(A[left]>=maxleft) maxleft=A[left];
+                else res+=maxleft-A[left];
+                left++;
+            }
+            else{
+                if(A[right]>=maxright) maxright= A[right];
+                else res+=maxright-A[right];
+                right--;
+            }
         }
-        
-        for(int i=0;i<n;i++){
-            ans+= min(greatest_to_left[i], greatest_to_right[i])- height[i];
-        }
-        
-        return ans;
-   
+        return res;
     }
 };
