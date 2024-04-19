1e126cb72a593a8d6dffbe4afcab75a6d97c6a18
Aditya Khadekar <79308024+Aditya20743@users.noreply.github.com>
2024-04-20 00:28:34 +0530

    Time: 10 ms (18.50%), Space: 15.9 MB (64.31%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,49 +1,52 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n= grid[0].size();
-        vector<vector<int>> vis(m,vector<int>(n,0));
+        queue<pair<int,int>> q;
+        int n=grid.size();
+        int m=grid[0].size();
         
-        queue<pair<int,pair<int,int>>> q;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j] == 2){
-                    q.push({0,{i,j}});
-                    vis[i][j]=1;
+        int fresh=0;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    q.push({i,j});
+                }
+                else if(grid[i][j]==1){
+                    fresh++;
                 }
             }
         }
+        // cout<<fresh<<endl;
         
-        vector<int> dir= {-1,0,1,0,-1};
-        int t_max=0;
-                           
+        if(fresh==0)        return 0;
+        int dir[5] = {-1,0,1,0,-1};
+        
+        int time= -1;
         while(!q.empty()){
-            int r= q.front().second.first;
-            int c= q.front().second.second;
-            int t= q.front().first;
-            vis[r][c]=1;
-            q.pop();
-            
-            t_max=max(t_max, t);
-            
-            for(int i=0;i<4;i++){
-                int nr= r+ dir[i];
-                int nc= c+ dir[i+1];
+            int qs = q.size();
+            for(int i=0;i<qs;i++){
+                int ri = q.front().first;
+                int rj = q.front().second;
+                q.pop();
                 
-                if(nr>=0 && nc>=0 && nr<m && nc<n && !vis[nr][nc] && grid[nr][nc]==1){
-                    q.push({t+1,{nr,nc}});
-                    vis[nr][nc]=1;
+                for(int i=0;i<4;i++){
+                    int ni = ri+ dir[i];
+                    int nj = rj+ dir[i+1];
+                    
+                    if(ni>=0 && ni<n && nj>=0 && nj<m){
+                        if(grid[ni][nj]==1){
+                            fresh--;
+                            q.push({ni,nj});
+                            grid[ni][nj]=2;
+                        }
+                    }
                 }
             }
+            time++;
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(!vis[i][j] && grid[i][j]==1){
-                    return -1;
-                }
-            }
-        }
-        return t_max;
+        if(fresh==0)     return time;
+        return -1;
+        
     }
 };
